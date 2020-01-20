8263da0575219b9cf2e0fc3fb56f05c4e4d9955c
Mikhail7788 <mikhail843@gmail.com>
2020-01-20 20:03:58 +0300

    add edits to the main.js for the task 2;3

--- 1.1-variables/main.js
+++ 1.1-variables/main.js
@@ -1,6 +1,19 @@
+//task 1
 let algebra = 5;
 let geography = 4;
 let physics = 4;
 
 let average = ((algebra + geography + physics)/3);
 console.log(average);
+
+//task2
+let myName = 'Михаил';
+let message = (`Привет, мир! Меня зовут ${myName}`);
+console.log(message);
+
+//task3
+let x = 2;
+let y = 22;
+let z = 0;
+let result = (x * y + 5 * z + x - 1);
+console.log(result);
