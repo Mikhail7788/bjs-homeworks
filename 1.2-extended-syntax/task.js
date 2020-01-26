7319a420e1ed88bbf565845a472dd281c3e2cae0
Mikhail7788 <mikhail843@gmail.com>
2020-01-26 09:36:33 +0300

    decision task 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -12,7 +12,20 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    //return x;
+    let dm = (Math.pow(b,2) - 4*a*c);
+    let x = [];
+    
+    if (dm < 0) 
+       return [];
+    if (dm === 0) 
+        xOne = ((-b) + Math.sqrt(dm))/(2*a);
+    if (dm > 0){
+        x[0] = ((-b) + Math.sqrt(dm))/(2*a);
+        x[1] = ((-b) -Math.sqrt(dm))/(2*a);
+    }
+    
+    return x;
+    
 }
 
 function calculateAverageRating(){
@@ -23,7 +36,16 @@ function calculateAverageRating(){
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    //return averageMark;
+    if (marks.length > 5) {
+    	console.log("Количество оценок больше 5");
+    	marks = marks.slice(0, 5);
+    }
+    let averageMark = 0;
+    for (let i=0; i < marks.length; i++) {
+    	averageMark += marks[i];
+    }
+    averageMark = (averageMark/marks.length);
+    return averageMark;
 }
 
 function calculateDrinkTask(){
@@ -37,4 +59,14 @@ function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
     //console.log(result)
     //return result;
+    let nowYear = new Date();
+    let age = nowYear.getFullYear() - dateOfBirthday.getFullYear();
+    let message;
+    if (age > 18) {
+    	message = `Не желаете ли олд-фэшн, ${name} ?`;
+    } else {
+    	message = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+    console.log(message);
+    return message;
 }
