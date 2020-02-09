bac85619d24a953cf04bd76483e9fdae37c0dde3
Mikhail7788 <mikhail843@gmail.com>
2020-02-09 08:46:55 +0300

    decision of the task wrappers

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -12,26 +12,40 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    //код для задачи №1 писать здесь
     
-     //if (percent > 0) {  
-        //percent = +percent;
-    if (percent <= 0 || typeof percent == 'string') {
-        return (`“Параметр "Процентная ставка" содержит неправильное значение ${percent}”.`);
-    }
- 
-    if (contribution < 0) {  
-        return (`“Параметр "Начальный взнос" содержит неправильное значение ${contribution}”.`); 
-    }
-   
-    if (amount <=  0) {
-          return (`“Параметр "Общая стоимость" содержит неправильное значение ${amount}”.`); 
-    }
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
+  if (isNaN(percent)) {
+    return `Параметр \`Процентная ставка\` содержит неправильное значение`;
+  } 
+  
+  if (isNaN(contribution)) {
+      return `Параметр \`Первоначальный взнос\` содержит неправильное значение`;
+  }
     
- }
-
+  if (isNaN(amount)) {
+      return `Параметр \`Сумма кредита\` содержит неправильное значение`;
+  }
+    
+    
+  let now = new Date;
+  let dueDate = new Date(date);
+  let dateNow = now.getTime();
+  let endMounth = dueDate.getTime() - dateNow;
+  let p = parseFloat(percent);
+  let monthlyPc = p / 12 / 100;
+  let months = Math.floor(endMounth / 60000 / 60 /24 /30 );
+  
+  let payment = amount - contribution;
+  
+  let payMonthly = (monthlyPc * (Math.pow(1 + monthlyPc, months))) / (Math.pow(1 + monthlyPc, months) - 1);
+  let totalAmount = payMonthly * payment * months;
+  return totalAmount.toFixed(2);
+     
+}
+    
 
 function sayHello() {
     let name = window.personName.value;
@@ -43,4 +57,10 @@ function sayHello() {
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     //return greeting;
+    if ( typeof name === 'undefined' || name === null || name === "" ) {
+        name = 'Аноним';
+    }
+    
+    let greeting = `Привет, мир! Меня зовут ${name}.`;
+    return greeting;
 }
